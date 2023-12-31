5a67bc872eb962a289e895998fbc67af197a4cba
Lahcsin Ayrahca <84768301+Nischal-Acharya@users.noreply.github.com>
2023-12-31 18:06:28 +0545

    Commited First Version v1.0

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include "nepLangCPP.h"
+
+int main(){
+    sankhya autaNumber;
+    sankhya arkoNumber;
+
+    vana_sathi << "Auta Number Deu: ";
+    maga_sathi >> autaNumber;
+
+    vana_sathi << "Arko Number Deu: ";
+    maga_sathi >> arkoNumber;
+
+    vana_sathi << "Timro Uttar " << autaNumber + arkoNumber << " aayo!";
+}
---
+++ nepLangCPP.h
@@ -0,0 +1,26 @@
+// Developed By Nishchal Acharya (Lahcsin) and Sahil Acharya
+#include <iostream>
+#define rakha #include
+#define vana_sathi std::cout
+#define maga_sathi std::cin
+#define yedi if
+#define vaynaVane else if
+#define natra else
+#define jaba_samma for
+#define jauJau continue;
+#define xordeu break;
+#define mamla case
+#define pahilakoi default
+#define sadhaiko_lagi const
+#define gara do
+#define jau goto
+#define sankhya int
+#define aanka float
+#define thuloAanka double
+#define dhacha struct
+#define katroXa sizeof
+#define samjeu static
+#define kastoXa typeof
+#define khali void
+#define jabaKi while
+using namespace std;
